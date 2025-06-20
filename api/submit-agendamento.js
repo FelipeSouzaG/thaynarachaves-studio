e0e60f20a32a621a36e7f7d825cb93ebb677a0d3
FelipeSouzaG <154008572+FelipeSouzaG@users.noreply.github.com>
2025-06-19 21:43:15 -0300

    Update submit-agendamento.js

--- api/submit-agendamento.js
+++ api/submit-agendamento.js
@@ -6,7 +6,10 @@ export default async function handler(request, response) {
   // Pega as variáveis de ambiente seguras
   const GOOGLE_API_URL = process.env.GOOGLE_API_URL;
   const BOT_TOKEN = process.env.TELEGRAM_BOT_TOKEN;
-  const CHAT_ID = process.env.TELEGRAM_CHAT_ID;
+  const CHAT_ID = [
+    process.env.TELEGRAM_CHAT_ID_ADMIN,
+    process.env.TELEGRAM_CHAT_ID_DEV,
+  ];
   const PHONE_ADMIN = process.env.ADMIN_PHONE;
 
   // Pega os dados enviados pelo front-end
@@ -28,34 +31,32 @@ export default async function handler(request, response) {
     const appointmentData = await googleRes.json();
 
     if (!googleRes.ok || appointmentData.status === 'error') {
-      // Se der erro no Google, já para por aqui
+
       return response.status(400).json(appointmentData);
     }
 
-    // 2. Enviar notificação para o Telegram
     const notificationMsg = `Novo agendamento:\nNome: ${nome}\nTelefone: ${telefone}\nProcedimento: ${procedimento}\nData: ${data}\nHora: ${horario}`;
-
-    try {
-      await fetch(`https://api.telegram.org/bot${BOT_TOKEN}/sendMessage`, {
-        method: 'POST',
-        headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify({
-          chat_id: CHAT_ID,
-          text: notificationMsg,
-        }),
-      });
-    } catch (telegramError) {
-      console.warn(
-        'Erro ao enviar para Telegram, gerando link do WhatsApp:',
-        telegramError
-      );
-      const msgWa = `Olá! Acabei de fazer um agendamento\nSegue os dados:\n${notificationMsg}`;
-      const encodedMsg = encodeURIComponent(msgWa);
-      // Adiciona o link do WhatsApp na resposta de sucesso
-      appointmentData.whatsappLink = `https://wa.me/${PHONE_ADMIN}?text=${encodedMsg}`;
+    for (const chat_id of CHAT_ID) {
+      try {
+        await fetch(`https://api.telegram.org/bot${BOT_TOKEN}/sendMessage`, {
+          method: 'POST',
+          headers: { 'Content-Type': 'application/json' },
+          body: JSON.stringify({
+            chat_id: chat_id,
+            text: notificationMsg,
+          }),
+        });
+      } catch (telegramError) {
+        console.warn(
+          'Erro ao enviar para Telegram, gerando link do WhatsApp:',
+          telegramError
+        );
+        const msgWa = `Olá! Acabei de fazer um agendamento\nSegue os dados:\n${notificationMsg}`;
+        const encodedMsg = encodeURIComponent(msgWa);
+        appointmentData.whatsappLink = `https://wa.me/${PHONE_ADMIN}?text=${encodedMsg}`;
+      }
     }
-
-    // Retorna a resposta de sucesso para o front-end
+    
     return response.status(200).json(appointmentData);
   } catch (error) {
     console.error('Erro no agendamento:', error);
